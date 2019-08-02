063f6f5495244447b7a249bcfef9e7011febe4c5
王富成 <wangfucheng@machloop.com>
2019-08-02 13:41:09 +0800

    Updates

---
+++ precache-manifest.0c48feb5688698064e614d6ef6949f00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a16b430d60d53bfd57ed4a3c7811451",
+    "url": "/better-scroll-react-demo/index.html"
+  },
+  {
+    "revision": "943a853cbe481489e25a",
+    "url": "/better-scroll-react-demo/static/css/main.732ba5db.chunk.css"
+  },
+  {
+    "revision": "9e877260bc15fedde917",
+    "url": "/better-scroll-react-demo/static/js/2.9245a0ca.chunk.js"
+  },
+  {
+    "revision": "943a853cbe481489e25a",
+    "url": "/better-scroll-react-demo/static/js/main.a6d6f698.chunk.js"
+  },
+  {
+    "revision": "8cac9a2b6ce16ad578c0",
+    "url": "/better-scroll-react-demo/static/js/runtime~main.0f23d9aa.js"
+  },
+  {
+    "revision": "8ea9318b222d559c977b93682ffa53fe",
+    "url": "/better-scroll-react-demo/static/media/iframe_iphonex.8ea9318b.png"
+  }
+]);
